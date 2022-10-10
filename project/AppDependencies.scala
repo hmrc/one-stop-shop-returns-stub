6744c0ed4803b1ba10a154f4a85f1c81ebb0d486
DevAndyroo <andy@andyroo.info>
2022-10-10 09:40:57 +0100

    VEOSS-1277 update library versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,13 +6,13 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.7.0",
     "com.github.fge"          %  "json-schema-validator"      % "2.2.6",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.66.0"
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.73.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.24.0"             % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.7.0"             % Test,
     "org.scalatest"           %% "scalatest"                  % "3.2.12"            % Test,
     "com.typesafe.play"       %% "play-test"                  % PlayVersion.current % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2"            % "test, it",
