63d02529540371613e6aaa504f89decdeac0cf14
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2024-02-14 15:17:15 +0000

    VEOSS-1779 | Upgrade to Play 3.0

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ lazy val microservice = Project(appName, file("."))
   .enablePlugins(play.sbt.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin)
   .settings(
     majorVersion                     := 0,
-    scalaVersion                     := "2.13.8",
+    scalaVersion                     := "2.13.12",
     libraryDependencies              ++= AppDependencies.compile ++ AppDependencies.test,
 
     RoutesKeys.routesImport ++= Seq(
@@ -19,3 +19,4 @@ lazy val microservice = Project(appName, file("."))
   .settings(integrationTestSettings(): _*)
   .settings(resolvers += Resolver.jcenterRepo)
   .settings(PlayKeys.playDefaultPort := 10206)
+  .settings(scalacOptions += "-Wconf:src=routes/.*:s")
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,15 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                 %% "bootstrap-backend-play-28"  % "7.15.0",
+    "uk.gov.hmrc"                 %% "bootstrap-backend-play-30"  % "8.4.0",
     "com.github.java-json-tools"  %  "json-schema-validator"      % "2.2.14"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.15.0"             % Test,
-    "org.scalatest"           %% "scalatest"                  % "3.2.12"            % Test,
-    "com.typesafe.play"       %% "play-test"                  % PlayVersion.current % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2"            % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "8.4.0"             % Test,
+    "org.scalatest"           %% "scalatest"                  % "3.2.15"            % Test,
+    "org.playframework"       %% "play-test"                  % PlayVersion.current % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.6"            % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"             % "test, it"
   )
 }
