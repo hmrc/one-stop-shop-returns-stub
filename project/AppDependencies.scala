b7c2f4e5f97078f13c7a4a5aebf2647c1de6c0b5
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2024-03-19 11:06:38 +0000

    VEOSS-1803 | Upgrade to Play 3.0 on OSS services

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,6 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "8.4.0"             % Test,
     "org.scalatest"           %% "scalatest"                  % "3.2.15"            % Test,
     "org.playframework"       %% "play-test"                  % PlayVersion.current % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.6"            % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"             % "test, it"
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.6"            % "test, it"
   )
 }
